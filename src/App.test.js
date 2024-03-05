da003b5c285ce4c2f6fddf69b496171e06049c03
Durg <durgesh_p@persistent.com>
2024-03-05 15:52:22 +0530

    Jest test case implementation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App';
+import Home from './components/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('to render text present', () => {
+//   render(
+//   <Router>
+//       <Home />
+//   </Router>);
+//   const text = screen.getByText(/Welcome Here!/i);
+//   expect(text).toBeInTheDocument();
+// });
+
+// test('to render text not present', () => {
+//   render(
+//   <Router>
+//       <Home />
+//   </Router>);
+//   const text2 = screen.getByText(/Welcome Everyone/i);
+//   expect(text2).toBeInTheDocument();
+// });
+
+describe("UI test case group",() =>{
+
+  test("test case 1",()=>{
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
+    let inputChek = screen.getAllByRole("textbox");
+    expect(inputChek[0]).toHaveAttribute("placeholder","Enter Your Name");
+    expect(inputChek[1]).toHaveAttribute("placeholder","Enter email");
+  })
+})
+
+test('to render password field present', () => {
+  render(
+  <Router>
+      <Home />
+  </Router>);
+  const pass = screen.getByPlaceholderText("password");
+  expect(pass).toBeInTheDocument();
 });
